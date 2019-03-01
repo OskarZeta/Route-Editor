cf58099cfd02eaaea95592755790d86664f2627f
Li Dian <dw4h@yandex.ru>
2019-03-01 17:09:57 +0300

    Upgraded & partially reworked application styling

--- src/index.js
+++ src/index.js
@@ -2,16 +2,5 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import './css/style.css';
 import App from './Components/App';
-import * as serviceWorker from './serviceWorker';
 
 ReactDOM.render(<App />, document.getElementById('root'));
-
-// If you want your app to work offline and load faster, you can change
-// unregister() to register() below. Note this comes with some pitfalls.
-// Learn more about service workers: http://bit.ly/CRA-PWA
-//serviceWorker.unregister();
-
-// <script
-//   src="https://api-maps.yandex.ru/2.1/?apikey=ea6dba4a-0251-4ffa-a9a4-bcb0e00e4732"
-//   type="text/javascript">
-// </script>
